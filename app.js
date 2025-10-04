cdf1ba1124d336916a7cfc592912568283dbe93d
unknown <franciscojavier.gutierrezrdgz@gmail.com>
2025-10-04 16:52:32 -0600

    modified styles.css

--- app.js
+++ app.js
@@ -11,16 +11,15 @@ btnAddTask.addEventListener("click", () => {
     li.textContent = taskText;
     taskList.appendChild(li);
     inputTask.value = ""; // limpiar input
+    document.getElementById("tasksAdded").style.visibility = 'visible';
   }
 });
 
 btnDeleteTask.addEventListener("click", () => {
    if(taskList){
     taskList.remove();
+    document.getElementById("tasksAdded").style.visibility = 'hidden';
    } else {
     console.log('Element not found.');
    }
-
-
-
 });
