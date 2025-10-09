993b278b99f977d9a76ec992b7ebaa1e72c8f4e6
unknown <franciscojavier.gutierrezrdgz@gmail.com>
2025-10-08 22:00:31 -0600

    added changes to files

--- app.js
+++ app.js
@@ -3,28 +3,40 @@ const inputTask = document.getElementById("taskInput");
 const btnAddTask = document.getElementById("bttnAddTask");
 const taskList = document.getElementById("taskList");
 const btnDeleteTask = document.getElementById("bttnDeleteTask");
-const bttnDeleteOneTask = document.getElementById("bttnDeleteOneTask");
+
 
 //Event adding a task
 btnAddTask.addEventListener("click", () => {
   const taskText = inputTask.value.trim();
     if(taskText !== ""){
     const li = document.createElement("li");
-    li.textContent = taskText;
-    taskList.appendChild(li);
-    const newButton = document.createElement('button');
-    newButton.textContent = "❌";
-    newButton.classList.add("deleteBtn");
-      // Agregar evento al botón de eliminar
-  newButton.addEventListener("click", () => {
+    //Agregamos clase para estilo
+    li.classList.add("taskItem"); 
+    // Crear el texto
+    const span = document.createElement("span");
+    span.textContent = taskText;
+    //Agregar botón para eliminar tarea
+    const newButtonDelteTask = document.createElement('button');
+    newButtonDelteTask.textContent = "✖️";
+    newButtonDelteTask.classList.add("deleteBtn");
+    //Agregar botón para marcar la tarea como hecha
+    const newButtonTaskCompleted = document.createElement('button');
+    newButtonTaskCompleted.textContent = "✔️";
+    newButtonTaskCompleted.classList.add("checkedBtn");
+    // Agregar evento al botón de eliminar tarea
+    newButtonDelteTask.addEventListener("click", () => {
     li.remove(); // elimina sólo esta tarea
   });
 
-  // Agregar el botón dentro del li
-  li.appendChild(newButton);
 
-  // Agregar el li a la lista
+  // Agregar todo al li
+  li.appendChild(span);
+  li.appendChild(newButtonDelteTask);
+  li.appendChild(newButtonTaskCompleted);
+
+  // Agregar el li al ul
   taskList.appendChild(li);
+
     inputTask.value = ""; // limpiar input
     document.getElementById("tasksAdded").style.visibility = 'visible';
   }
